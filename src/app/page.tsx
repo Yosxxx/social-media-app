e11f5489a898ffb191146ad05dd5b65da1069f66
Yos <Yosuamontolaluxd@gmail.com>
2025-06-06 14:48:54 +0700

    Navbar Components + Dark Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button className="bg-zinc-500 text-white p-5">Sign In</button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <h1>Home Page Content</h1>
     </div>
   );
 }
